f6491c63bd99c87c4068751089e4e71c2951a4d3
Lipika Kumar <lkumar@dandb.com>
2014-10-26 15:19:11 -0700

    removing extra broadcast reciever in updateService

--- app/src/main/java/com/procrastmenotapp/lkumar/procrastmenot/UpdateService.java
+++ app/src/main/java/com/procrastmenotapp/lkumar/procrastmenot/UpdateService.java
@@ -13,11 +13,7 @@ public class UpdateService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        // REGISTER RECEIVER THAT HANDLES SCREEN ON AND SCREEN OFF LOGIC
-        IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
-        filter.addAction(Intent.ACTION_SCREEN_OFF);
-        BroadcastReceiver mReceiver = new ScreenReceiver();
-        registerReceiver(mReceiver, filter);
+
     }
 
 
